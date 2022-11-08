eb1cc587a135470073c554f781071be5b621fcfc
owldaylong <anastasiameicy@gmail.com>
2022-11-08 17:39:20 +0700

    Added feature register & login user

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -13,6 +13,53 @@ class Controller {
 			next(err);
 		}
 	}
+
+	static async register(req, res, next) {
+		try {
+			let { username, email, password } = req.body;
+			let createdUser = await User.create({
+				username,
+				email,
+				password,
+			});
+
+			res.status(201).json({
+				id: createdUser.id,
+				email: createdUser.email,
+			});
+		} catch (err) {
+			next(err);
+		}
+	}
+
+	static async login(req, res, next) {
+		try {
+			let { email, password } = req.body;
+
+			if (!email || !password) throw { name: "Bad Request" };
+
+			let findUser = await User.findOne({ where: { email } });
+
+			if (!findUser) throw { name: "Unauthorized" };
+
+			const comparedPassword = comparePassword(password, findUser.password);
+
+			if (!comparedPassword) throw { name: "Unauthorized" };
+
+			const payload = {
+				id: findUser.id,
+				role: findUser.role,
+			};
+
+			const access_token = createToken(payload);
+
+			res
+				.status(200)
+				.json({ access_token, role: findUser.role, email: findUser.email });
+		} catch (err) {
+			next(err);
+		}
+	}
 }
 
 module.exports = Controller;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const Controller = require("../controllers/controllers");
 
+const register = require("./register");
+const login = require("./login");
+
+// ACCOUNTS
 router.get("/", Controller.home);
+router.use("/register", register);
+router.use("/login", login);
 
 module.exports = router;
