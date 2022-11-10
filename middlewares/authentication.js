fcc2c5151c279ec7b1796345860e7d5183d12410
owldaylong <anastasiameicy@gmail.com>
2022-11-10 09:18:25 +0700

    added google login

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,17 +3,18 @@ const { User } = require("../models");
 
 async function authentication(req, res, next) {
 	try {
+		console.log(req.headers);
 		let access_token = req.headers.access_token;
 		if (!access_token) throw { name: "Unauthorized" };
 
 		let payload = verifyToken(access_token);
+
 		let user = await User.findByPk(payload.id);
 
 		if (!user) throw { name: "Unauthorized" };
 
 		req.user = {
 			id: user.id,
-			role: user.role,
 			email: user.email,
 		};
 		next();
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,17 @@ const Controller = require("../controllers/controllers");
 
 const register = require("./register");
 const login = require("./login");
+const googleLogin = require("./google-login");
 const beverages = require("./beverages");
 const transactions = require("./transactions");
+
 const authentication = require("../middlewares/authentication");
 
 // ACCOUNTS
 router.get("/", Controller.home);
 router.use("/register", register);
 router.use("/login", login);
+router.use("/google-login", googleLogin);
 
 // MAIN INTERFACE - CONSUMER
 router.use("/beverages", beverages);
